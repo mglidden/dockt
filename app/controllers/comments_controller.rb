80379b3ca7df6294219c089dbadd301b5fc18b3e
Mason Glidden <mglidden@mit.edu>
2012-01-11 15:14:30 -0500

    can now create comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,10 @@
 class CommentsController < ApplicationController
+  def create
+    @document = Document.find(params[:document_id])
+    @comment = @document.comments.create(params[:comment])
+    redirect_to document_path(@document)
+  end
+
   def show
     @comment = Comment.find(params[:id])
 
