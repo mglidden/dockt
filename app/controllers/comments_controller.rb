e5ea677c8d18b61b2cc8747ceb116fa437e94edd
Mason Glidden <mglidden@mit.edu>
2012-01-11 15:05:17 -0500

    properly show groups, documents, comments on nested pages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+  def show
+    @comment = Comment.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @document }
+    end
+  end
 end
--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -4,4 +4,14 @@ class DocumentsController < ApplicationController
     @document = @group.documents.create(params[:document])
     redirect_to group_path(@group)
   end
+  
+  def show
+    @document = Document.find(params[:id])
+    @comments = @document.comments
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @document }
+    end
+  end
 end
