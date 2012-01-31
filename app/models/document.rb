722247ace263b78ce654deb706b792eb3149f387
Mason Glidden <mglidden@mit.edu>
2012-01-31 11:53:15 -0500

    fixed pages collection error

--- app/models/document.rb
+++ app/models/document.rb
@@ -11,8 +11,12 @@ class Document < ActiveRecord::Base
   def get_pages
     pages_cmd = IO.popen('ls public/docs/ | grep ^' + self.id.to_s + '-')
     pages = pages_cmd.readlines.collect {|file| ['/docs/' + file[0..-2], file.split('-')[1].to_i]}
-    pages_cmd = IO.popen('ls public/dors/ | grep ^' + self.id.to_s + '.png');
+    puts pages
+    pages_cmd = IO.popen('ls public/docs/ | grep ^' + self.id.to_s + '.png');
     pages += pages_cmd.readlines.collect {|file| ['/docs/' + file[0..-2], file.split('-')[1].to_i]}
+    puts pages
+    puts 'here'
+    puts '\n\n\n'
 
     pages.sort_by! { |url, filenum| filenum }
     return pages
