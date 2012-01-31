17a0a1607ca73aea83577fa0e6b58c67f81daaf3
Mason Glidden <mglidden@mit.edu>
2012-01-31 12:28:42 -0500

    front pages changes, small bug fix

--- app/models/document.rb
+++ app/models/document.rb
@@ -11,8 +11,9 @@ class Document < ActiveRecord::Base
   def get_pages
     pages_cmd = IO.popen('ls public/docs/ | grep ^' + self.id.to_s + '-')
     pages = pages_cmd.readlines.collect {|file| ['/docs/' + file[0..-2], file.split('-')[1].to_i]}
-    pages_cmd = IO.popen('ls public/docs/ | grep ^' + self.id.to_s + '.png');
-    pages += pages_cmd.readlines.collect {|file| ['/docs/' + file[0..-2], file.split('-')[1].to_i]}
+    if pages.length == 0
+      pages = ['/docs/'+self.id.to_s+'.png']
+    end
 
     pages.sort_by! { |url, filenum| filenum }
     return pages
